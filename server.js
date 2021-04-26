0a05281da59d68f10d560552f594a862a0ef581d
DianaTrujillo-create <diana.trujillo0806@gmail.com>
2021-04-26 17:19:05 -0500

    commit to main

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+
+// Initialize the app 
+const app = express(); 
+const PORT = process.env.PORT | 8000;
