54a83fb227d3d218997f3823007bfbce746d0a7b
DianaTrujillo-create <diana.trujillo0806@gmail.com>
2021-04-26 19:28:56 -0500

    commit to main

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,3 +9,11 @@ router.get('/notes', (req, res) => {
         })
         .catch((err) => res.status(500).json(err));
 });
+
+router.post('/notes', (req, res) => {
+    store
+        .addNote(req.body)
+        .then((note) => res.json(note))
+        .catch((err) => res.status(500).json(err));
+});
+
