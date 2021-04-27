ccd006dfefc9256662cd89706680c0b4d2209b99
DianaTrujillo-create <diana.trujillo0806@gmail.com>
2021-04-26 20:39:00 -0500

    commit to main

---
+++ db/store.js
@@ -0,0 +1,39 @@
+const util = require('util');
+const fs = require('fs');
+
+const uuidv1 = require('uuid/v1');
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+class Store {
+    read() {
+        return readFileAsync('db/db.json', 'utf8');
+    }
+
+    write(note) {
+        return writeFileAsync('db/db.json', JSON.stringify(note));
+    }
+
+    getNotes() {
+        return this.read().then((notes) => {
+            let parsedNotes;
+
+            try {
+               parsedNotes = [].concat(JSON.parse(notes));
+            }  catch (err) {
+               parsedNotes = [];
+            }
+
+            return parsedNotes;
+        });
+    }
+    
+    addNote(note) {
+        const { title, text } = note;
+
+        if (!title || !text) {
+            throw new Error("Note 'title' and 'text' cannot be blank");
+        }
+    }
+}
