5c18a8fe18e3124d2d9f56126d52812e94a760b7
DianaTrujillo-create <diana.trujillo0806@gmail.com>
2021-05-01 18:17:15 -0500

    fixed routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,30 +1,68 @@
-let data = require('../db/db.json');
 const fs = require('fs');
 const path = require('path');
+const { all } = require('sequelize/types/lib/operators');
 
 module.exports = function(app) {
-    app.get('/api/notes', function(req, res){
-        let notes = JSON.parse(fs.readFileSync(path.join(__dirname, '../db/db.json')));
+    app.get('/api/notes', function(req, res) {
+        fs.readFileSync(path.join(__dirname, '../db/db.json'), "utf8", function(err,data) {
+            if (err) throw err;
+            res.json(JSON.parse(data));
+            console.log(data)
+        })
+        return res.status(200);
     });
 
-app.post('/api/notes', function(req,res){
-    notes.push(req.body);
-    var updatedNotes = JSON.stringify(notes);
-    fs.writeFileSync('./db/db.json', updatedNotes);
-    res.json(req.body);
-});
+        app.post('/api/notes', function(req, res) {
+  
+            fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", function(err, data) {
+                if (err) throw err;
 
-    app.delete('/api/notes/:id', function(req,res){
-        var noteID = req.params.id;
-        let notes = JSON.parse(fs.readFileSync(path.join(_dirname, '../db/db.json')));
-        let index = note.findIndex( element => {
-            if (element.id === noteID) return true;
+                const allNotes = JSON.parse(data);
+                console.log(allNotes);
+                const newNotes = [];
+
+                allNotes.push(req.body);
+                console.log(allNotes)
+
+                for(let i = 0; i < allNotes.lenght; i++) {
+                    const addNote = {
+                        title: allNotes[i].title,
+                        text: allNotes[i].text,
+                        id: newNotes.length
+                    };
+                    
+                    newNotes.push(addNote);
+                    console.log(newNotes);
+                }
+
+                fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(newNotes, null, 2), function(err) {
+                    if (err) throw err;
+                    res.json(req.body);
+                    console.log(newNotes);
+                    console.log(allNotes)
+                });
+                return res.status(200)
+            });
         });
-        notes.splice(index, 1);
+    
+        app.delete('/api/notes/:id', function(req,res) {
+        
+            fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", function(err, data) {
+                if (err) throw err;
 
-        var updatedNotes = JSON.stringify(notes);
-        fs.writeFileSync('./db/db.json', updatedNotes);
+                let allNotes = JSON.parse(data);
+                console.log(JSON.stringify(allNotes) + "will be deleted");
 
-        res.send('Deleting note');
-    });
+                allNotes = allNotes.filter(function(data) {
+                    return data.id !=req.params.id;
+                });
+
+                fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify (allNotes, null, 2), function(err) {
+                    if (err) throw err;
+                    res.json(req.body);
+                    });
+                });
+
+                return res.status(200)
+            });
 }
